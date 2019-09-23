1c0f81eb904f00c55031dc39a0535fd5e2a2b1b1
Helder Doutel <helder.gomes@nube.com.br>
2019-09-23 13:07:02 -0300

    flask

--- sevenclicks.py
+++ sevenclicks.py
@@ -1,1 +1,9 @@
-return True
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "Hello World! <strong>I am learning Flask</strong>", 200
+
+app.run()
