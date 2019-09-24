1d7057e41c91243432d65a605c7651833376be12
Helder Doutel <helder.gomes@nube.com.br>
2019-09-24 17:43:13 -0300

    Tratamento do request de pagina especifica e home

--- sevenclicks.py
+++ sevenclicks.py
@@ -1,13 +1,24 @@
 import os
 import requests
-from flask import Flask
+from flask import Flask, render_template, request
+import re
 
 app = Flask(__name__)
 
+@app.route("/pagina")
+def rende_pagina():
+    url = request.args.get(
+        'url', 'https://pt.wikipedia.org/wiki/Especial:Aleat%C3%B3ria')
+    r = None
+    if re.match(r'http(s)+://\w\w.wikipedia.org/(\S+|)+', url):
+        r = requests.get(url)
+    if r:
+        return r.text.replace('href="/', 'href="https://pt.wikipedia.org/')
+    return "Ops"
+
 @app.route("/")
-def hello_world():
-    r = requests.get('https://pt.wikipedia.org/wiki/Especial:Aleat%C3%B3ria')
-    return r.text.replace('href="/', 'href="https://pt.wikipedia.org/')
+def home():
+    return "Calmaa!"
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
