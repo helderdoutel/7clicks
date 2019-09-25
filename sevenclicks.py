e9e1db2090e970758b391a7774356adb20554cf0
Lucas <lucaslou4@gmail.com>
2019-09-25 17:49:27 -0300

    tira o debug pro ambiente de prod

--- sevenclicks.py
+++ sevenclicks.py
@@ -66,4 +66,4 @@ def pagina_wiki(page):
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=port)
