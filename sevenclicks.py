53e078863ab9939a9eb9905726d6572cd9868fd6
gabriel diniz gisoldo <gabrielgisoldi@gmail.com>
2019-09-26 22:22:08 -0300

    Update sevenclicks.py

--- sevenclicks.py
+++ sevenclicks.py
@@ -22,28 +22,19 @@ def home():
 @app.route("/start", methods=["POST"])
 def iniciar():
     """Funcao para iniciar o jogo."""
-    mobile = int(request.form.get('mobile', 0))
+    page_start = request.form.get(
+        'page_start', 'https://pt.wikipedia.org/wiki/Especial:Aleat%C3%B3ria')
 
-    session['mobile'] = mobile
-
-    if mobile:
-        url_default = 'https://pt.wikipedia.org/wiki/Especial:Aleat%C3%B3ria'
-    else:
-        url_default = 'https://pt.m.wikipedia.org/wiki/Especial:Aleat%C3%B3ria'
-
-    page_start = request.form.get('page_start', url_default)
-
-    page_end = request.form.get('page_end', url_default)
+    page_end = request.form.get(
+        'page_end', 'https://pt.wikipedia.org/wiki/Especial:Aleat%C3%B3ria')
 
     if not page_start or\
-            not re.match(r'http(s)+://\w\w\.(|m\.)wikipedia.org/(\S+|)+',
-                         page_start):
-        page_start = url_default
+            not re.match(r'http(s)+://\w\w.wikipedia.org/(\S+|)+', page_start):
+        page_start = 'https://pt.wikipedia.org/wiki/Especial:Aleat%C3%B3ria'
 
     if not page_end or\
-            not re.match(r'http(s)+://\w\w\.(|m\.)wikipedia.org/(\S+|)+',
-                         page_end):
-        page_end = url_default
+            not re.match(r'http(s)+://\w\w.wikipedia.org/(\S+|)+', page_end):
+        page_end = 'https://pt.wikipedia.org/wiki/Especial:Aleat%C3%B3ria'
 
     page_start = requests.get(page_start)
     url_start = urllib.parse.unquote(page_start.url)
@@ -53,30 +44,17 @@ def iniciar():
     session['url_end'] = url_end
 
     if page_start:
-        if mobile:
-            text = page_start.text.replace(
-                'href="/', 'href="https://pt.m.wikipedia.org/')
-        else:
-            text = page_start.text.replace(
-                'href="/', 'href="https://pt.wikipedia.org/')
+        text = page_start.text.replace(
+            'href="/', 'href="https://pt.wikipedia.org/')
 
     return jsonify({"text": text, "destino": url_end, 'inicio': url_start})
 
 
-@app.route('/random',  methods=["POST", "GET"])
+@app.route('/random')
 def pegar_aleatoria():
     """Retorna pagina aleatoria."""
-    mobile = int(request.form.get('mobile', 0))
-
-    session['mobile'] = mobile
-
-    if mobile:
-        url_default = 'https://pt.wikipedia.org/wiki/Especial:Aleat%C3%B3ria'
-    else:
-        url_default = 'https://pt.m.wikipedia.org/wiki/Especial:Aleat%C3%B3ria'
-
-    page = requests.get(url_default)
-
+    page = requests.get(
+        'https://pt.wikipedia.org/wiki/Especial:Aleat%C3%B3ria')
     url = urllib.parse.unquote(page.url)
     return jsonify({"url": url})
 
@@ -84,18 +62,12 @@ def pegar_aleatoria():
 @app.route('/wiki/<page>')
 def pagina_wiki(page):
     """Busca a pagina do wikipedia do link clicado."""
-    if 'mobile' in session and session['mobile'] == 1:
-        url_default = 'https://pt.wikipedia.org/wiki/'
-        url_sub = 'href="https://pt.wikipedia.org/'
-    else:
-        url_default = 'https://pt.m.wikipedia.org/wiki/'
-        url_sub = 'href="https://pt.m.wikipedia.org/'
-
-    url = request.args.get('url', url_default + page)
-    if re.match(r'http(s)+://\w\w\.(|m\.)wikipedia.org/(\S+|)+', url):
+    url = request.args.get(
+        'url', 'https://pt.wikipedia.org/wiki/' + page)
+    if re.match(r'http(s)+://\w\w.wikipedia.org/(\S+|)+', url):
         r = requests.get(url)
     if r:
-        r = r.text.replace('href="/', url_sub)
+        r = r.text.replace('href="/', 'href="https://pt.wikipedia.org/')
 
     if url == session['url_end']:
         end = 1
@@ -103,7 +75,6 @@ def pagina_wiki(page):
         end = 0
 
     return jsonify({"text": r, "atingiu_destino": end})
-
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
